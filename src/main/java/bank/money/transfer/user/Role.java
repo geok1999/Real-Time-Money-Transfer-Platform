8cf4ad9d999fbb47e2444f7e45623871a6f8f13d
geok1999 <kalfara13@gmail.com>
2025-09-28 21:51:04 +0300

    refactor: Add Dependencies & Setup and User Entity & Repository for implementing JWT security

---
+++ src/main/java/bank/money/transfer/user/Role.java
@@ -0,0 +1,6 @@
+package bank.money.transfer.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/bank/money/transfer/user/User.java
@@ -0,0 +1,46 @@
+package bank.money.transfer.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
