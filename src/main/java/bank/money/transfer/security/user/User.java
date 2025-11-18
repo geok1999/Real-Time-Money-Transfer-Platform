77e7d516cd1c3503e6c1f81fbe54b532229bd945
geok1999 <kalfara13@gmail.com>
2025-11-18 23:00:23 +0200

    feature: add  validations at user class

--- src/main/java/bank/money/transfer/security/user/User.java
+++ src/main/java/bank/money/transfer/security/user/User.java
@@ -1,6 +1,8 @@
 package bank.money.transfer.security.user;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,13 +23,28 @@ import java.util.List;
 public class User implements UserDetails {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100)
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @Column(nullable = false, length = 100)
+    @NotBlank(message = "Last name is required")
     private String lastName;
+
+    @Column(nullable = false, unique = true, length = 255)
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Password is required")
     private String password;
+
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
 
     @Override
